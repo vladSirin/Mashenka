90214c6d3e44d733d133056c2fe8d49cd23b9e7f
Sirin <shadowsong_sirin@hotmail.com>
2024-01-02 15:00:35 +0800

    fix compilation issues
    adding warning suppressing
    update imgui

--- Mashenka/src/mkpch.h
+++ Mashenka/src/mkpch.h
@@ -1,5 +1,10 @@
 ﻿#pragma once
 
+// Suppress warnings
+#pragma warning(disable : 4251) //supress warnings about dll interface
+#define _SILENCE_ALL_MS_EXT_DEPRECATION_WARNINGS
+#define _SILENCE_STDEXT_ARR_ITERS_DEPRECATION_WARNING
+
 #include <iostream>
 #include <memory>
 #include <utility>
@@ -19,3 +24,4 @@
 #endif
 
 #define IMGUI_IMPL_OPENGL_LOADER_CUSTOM "Mashenka/Vendor/Glad/include/glad/glad.h"
+
