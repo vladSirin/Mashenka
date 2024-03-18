09bd85282ce5f3c62e5c423dbd42a477200d4c0c
Sirin <shadowsong_sirin@hotmail.com>
2024-03-18 21:27:02 +0800

    Submit random class first

---
+++ Sandbox/src/Random.cpp
@@ -0,0 +1,4 @@
+﻿#include "Random.h"
+
+std::mt19937 Random::s_RandomEngine; // Definition of the static member variable.
+// Note: The actual methods (Init, Float, Int) are implemented inline within the class definition and thus do not need separate definitions in this .cpp file.
---
+++ Sandbox/src/Random.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <random>
+
+class Random {
+public:
+    // Initializes the random engine. Call this at the start of your program.
+    static void Init() {
+        s_RandomEngine.seed(std::random_device()());
+    }
+
+    // Generates a floating-point number between 0.0 and 1.0.
+    static float Float() {
+        std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
+        return distribution(s_RandomEngine);
+    }
+
+    // Generates an integer number within [min, max].
+    static int Int(int min, int max) {
+        std::uniform_int_distribution<std::mt19937::result_type> distribution(min, max);
+        return distribution(s_RandomEngine);
+    }
+
+private:
+    static std::mt19937 s_RandomEngine;
+};
